3e9b6e7ca56b14b11707fc0f8ac33fb7f8f1dee3
ystays <85421593+ystays@users.noreply.github.com>
2023-09-26 10:03:09 +0800

    first test

--- src/signaling.js
+++ src/signaling.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:4000',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
 });
